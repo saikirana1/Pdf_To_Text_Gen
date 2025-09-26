efd67294d24667e63c7c62a9ab5aeaf1e0cf6e1f
saikirana1 <kasukurthisaikiran@89>
2025-09-26 19:25:46 +0530

    chore:fixed the index.py file

--- index.py
+++ index.py
@@ -22,11 +22,3 @@ app.include_router(router.router)
 
 
 
-if __name__ == "__main__":
-    print("Starting webserver...")
-    uvicorn.run(
-        app,
-        host="0.0.0.0",
-        port=int(os.getenv("PORT", 8080)),
-        proxy_headers=True
-    )
